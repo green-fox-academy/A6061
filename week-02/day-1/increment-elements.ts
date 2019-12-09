845baa5c70a3006597ef2242227a93bc90654bf9
Adam Juhasz <j.adam.lazlo@gmail.com>
2019-12-09 15:04:19 +0100

    Added additional help to diamond/pyramid and also uploaded 10 exercises from week2-day1

---
+++ week-02/day-1/increment-elements.ts
@@ -0,0 +1,8 @@
+'use strict';
+// -  Create a variable named `numList` with the following content: `[1, 2, 3, 4, 5]`
+// -  Increment the third element simply by accessing it
+// -  Log the third element to the console
+
+let numList1: number[] = [1, 2, 3, 4, 5]
+
+console.log(numList1[2]+1);
