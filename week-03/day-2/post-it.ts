4e2a3c7138f36cf23b9d05e085435fae299981cc
Adam Juhasz <j.adam.lazlo@gmail.com>
2019-12-17 12:19:54 +0100

    added and completed post-it.ts

---
+++ week-03/day-2/post-it.ts
@@ -0,0 +1,28 @@
+'use strict';
+
+// an orange with blue text: "Idea 1"
+// a pink with black text: "Awesome"
+// a yellow with green text: "Superb!"
+
+class PostIt {
+    backgroundColor: string;
+    text: string;
+    textColor: string;
+
+    constructor(backgroundColor: string, text: string, textColor: string) {
+        this.backgroundColor = backgroundColor;
+        this.text = text;
+        this.textColor = textColor;
+    }
+
+}
+
+let post1 = new PostIt('Black', 'Awesome', 'Yellow');
+let post2 = new PostIt('Magenta', 'Awesome', 'Green');
+let post3 = new PostIt('Orange', 'Awesome', 'Black');
+
+console.log(post1);
+
+console.log(post2);
+
+console.log(post3);
