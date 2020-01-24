22474b6508ae94461b75e4045c8e7aeee043c94f
Adam Juhasz <j.adam.lazlo@gmail.com>
2020-01-24 14:25:29 +0100

    added a few exercises from day 1 and something from the Gallery project..

---
+++ week-06/day-5/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+// When the user clicks on <div>, open the popup
+function myFunction() {
+  var popup = document.getElementById("descriptionPopup");
+  popup.classList.toggle("show");
+}
